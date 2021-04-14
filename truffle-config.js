1357888596a2c03b56e8cdc66a07c61639c17105
skundu42 <skundu_be19@thapar.edu>
2021-04-15 00:21:10 +0530

    final deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,29 @@
-const path = require("path");
-
+ //require('babel-register');
+ //require('babel-polyfill');
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
 module.exports = {
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(`mystery knee invite surface strike student lottery author medal bridge cricket trumpet`, `https://rinkeby.infura.io/v3/fd51fc7e7565472e87d8ab4a2ba964db`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    
+  },
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
-};
+}
